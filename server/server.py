be58196f18c64422a76dbcf95b8f0d43f0c90005
Anders Pitman <tapitman11@gmail.com>
2017-05-04 15:13:52 -0700

    Implement some API endpoints

--- server/server.py
+++ server/server.py
@@ -1,8 +1,10 @@
-from flask import Flask, jsonify, request
+import os
+import json
+from flask import Flask, send_from_directory, jsonify, request
 from pymongo import MongoClient
 from urllib.parse import quote_plus, unquote_plus
 from datetime import datetime
-import json
+from pprint import pprint
 
 
 app = Flask(__name__)
@@ -32,28 +34,98 @@ class DbManager(object):
         return self._db
 
 
+#client_dir = os.path.join(os.pardir, 'client')
+client_dir = os.getcwd()
+
 db = DbManager().get_db()
+
+@app.route('/')
+def index():
+    return send_from_directory(client_dir, 'index.html')
+
+@app.route('/<filename>')
+def get_static_file(filename):
+    return send_from_directory(client_dir, filename)
 
 @app.route('/upload_interval/')
 def upload_interval():
     return str(60) 
 
-@app.route('/readings/<int:sensor_id>', methods=['POST'])
-def reading(sensor_id):
-    #print(request.form['title'])
-    print(request.json)
+@app.route('/readings')
+def readings_all():
 
-    data = request.json
+    pipeline = [
+        {
+            # Collect readings by sensor_id
+            '$group': {
+                '_id': '$sensor_id',
+                'readings': {
+                    '$push': {
+                        'temperature_celsius': '$temperature_celsius',
+                        'humidity': '$humidity',
+                        'datetime': '$datetime',
+                    }
+                }
+            }
+        },
+        {
+            # Rename _id to sensor_id
+            '$project': {
+                '_id': 0,
+                'sensor_id': '$_id',
+                'readings': 1
+            }
+        },
+        {
+            # Sort ascending by sensor_id
+            '$sort': { 'sensor_id': 1 }
+        }
+    ]
 
-    reading = {
-        'sensor_id': sensor_id,
-        'temperature_celsius': data['temperature_celsius'],
-        'humidity': data['humidity'],
-        'datetime': datetime.now().isoformat(),
-    }
-    db.readings_collection.insert_one(reading)
+    cursor = db.readings_collection.aggregate(pipeline)
 
-    return "OK"
+    results = [ reading for reading in cursor ]
+
+    return jsonify(results)
+
+@app.route('/readings/<int:sensor_id>', methods=['GET', 'POST'])
+def readings(sensor_id):
+
+    if request.method == 'GET':
+
+        query = {
+            'sensor_id': sensor_id
+        }
+
+        cursor = db.readings_collection.find(query)
+
+        results = []
+        for reading in cursor:
+            result = {
+                'sensor_id': sensor_id,
+                'temperature_celsius': reading['temperature_celsius'],
+                'humidity': reading['humidity'],
+                'datetime': reading['datetime'],
+            }
+            results.append(result)
+
+        return jsonify(results)
+
+    elif request.method == 'POST':
+        #print(request.form['title'])
+        print(request.json)
+
+        data = request.json
+
+        reading = {
+            'sensor_id': sensor_id,
+            'temperature_celsius': data['temperature_celsius'],
+            'humidity': data['humidity'],
+            'datetime': datetime.now().isoformat(),
+        }
+        db.readings_collection.insert_one(reading)
+
+        return "OK"
 
 if __name__ == '__main__':
 
